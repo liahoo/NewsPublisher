1d31cb984e279f9925a3eba14baf9667c5c21b13
Liang Hu <liahoo@gmail.com>
2018-08-06 13:19:24 +0900

    News Publish v0.0.3 Release Note 1, Change Ads order 2, Change link URL for each link

--- app/src/main/java/com/appsflyer/support/publisher/NewsActivity.kt
+++ app/src/main/java/com/appsflyer/support/publisher/NewsActivity.kt
@@ -7,10 +7,14 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.format.DateFormat
+import android.text.format.DateUtils
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_news.*
+import java.net.URL
 import java.net.URLEncoder
+import java.util.*
 
 class NewsActivity : Activity(), ItemClickSupport.OnItemClickListener, ItemClickSupport.OnItemLongClickListener {
     override fun onItemLongClicked(recyclerView: RecyclerView, position: Int, v: View): Boolean {
@@ -18,9 +22,9 @@ class NewsActivity : Activity(), ItemClickSupport.OnItemClickListener, ItemClick
         recyclerAdapter.get(position)?.targetUrl?.let {
             AlertDialog.Builder(this)
                     .setMessage(it)
-                    .setPositiveButton(android.R.string.ok, { dialog, _ ->
+                    .setPositiveButton(android.R.string.ok) { dialog, _ ->
                         dialog.dismiss()
-                    })
+                    }
                     .create().show()
         }
         return true
@@ -29,17 +33,19 @@ class NewsActivity : Activity(), ItemClickSupport.OnItemClickListener, ItemClick
     override fun onItemClicked(recyclerView: RecyclerView, position: Int, v: View) {
         if(recyclerAdapter.getItemViewType(position)==0) return
         recyclerAdapter.get(position)?.targetUrl?.let {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(it)))
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse(it.replace("{epoch-time-adid}",(Date().time).toString() + "-" + PublisherApp.gaid))
+            ))
         }
     }
 
     fun onLongClicked(item: ClickItem): Boolean {
         item.targetUrl?.let {
             AlertDialog.Builder(this)
-                    .setMessage(it)
-                    .setPositiveButton(android.R.string.ok, { dialog, _ ->
+                    .setMessage(it.replace("{epoch-time-adid}",(Date().time).toString() + "-" + PublisherApp.gaid))
+                    .setPositiveButton(android.R.string.ok) { dialog, _ ->
                         dialog.dismiss()
-                    })
+                    }
                     .create().show()
         }
         return true
@@ -55,24 +61,18 @@ class NewsActivity : Activity(), ItemClickSupport.OnItemClickListener, ItemClick
 
     lateinit var newsList: List<ClickItem>
     private fun initItems(){
-        val baseUrl = "https://app.appsflyer.com/com.candyapp.appsflyer?pid=masterclass&c=publisherapps&af_adset=dlnow&af_click_lookback=1h"
-        val append = "&af_r=${URLEncoder.encode(BuildConfig.DOWNLOAD_URL, "UTF-8")}&advertising_id=${PublisherApp.gaid}"
+        val baseUrl = "http://bit.do/es7Bt"
         newsList = listOf(
                 ClickItem(0, getString(R.string.article_text_1), R.mipmap.skittlesstill),
-                ClickItem(1, "Download the Candy Shopping App Now", null,
-                        baseUrl + "&af_ad=General&af_adset_id=t1&af_siteid=123&af_cost_currency=USD&af_cost_value=1${append}"),
+                ClickItem(1, null, R.mipmap.mm, baseUrl + "?clickid={epoch-time-adid}"),
                 ClickItem(0, getString(R.string.article_text_2), R.mipmap.salted_caramel),
-                ClickItem(1, "Buy M&Ms in an App", null,
-                        baseUrl + "&af_ad=MM&af_adset_id=t2&af_siteid=123&af_cost_currency=USD&af_cost_value=1${append}"),
+                ClickItem(1, "Buy Skittles in an App", null, baseUrl + "?clickid={epoch-time-adid}"),
                 ClickItem(0, getString(R.string.article_text_3), R.mipmap.ruby_chocolate_barry_callebaut),
-                ClickItem(1, "Buy Skittles in an App", null,
-                        baseUrl + "&af_ad=Skittles&af_adset_id=t3&af_siteid=123&af_cost_currency=USD&af_cost_value=1${append}"),
+                ClickItem(1, "Buy M&Ms in an App", null, baseUrl + "?advertising_id=${PublisherApp.gaid}&clickid={epoch-time-adid}"),
                 ClickItem(0, getString(R.string.article_text_4), R.mipmap.jelly_belly_beer),
-                ClickItem(1, null, R.mipmap.mm,
-                        baseUrl + "&af_ad=MM&af_adset_id=iMM&af_siteid=123&af_cost_currency=USD&af_cost_value=2${append}"),
+                ClickItem(1, "Download the Candy Shopping App Now", null, baseUrl + "?advertising_id=${PublisherApp.gaid}&clickid={epoch-time-adid}"),
                 ClickItem(0, getString(R.string.article_text_5), R.mipmap.pg_18_3d_printer),
-                ClickItem(1, null, R.mipmap.skittles,
-                        baseUrl + "&af_ad=Skittles&af_adset_id=iSK&af_siteid=123&af_cost_currency=USD&af_cost_value=2${append}")
+                ClickItem(1, null, R.mipmap.skittles, baseUrl + "?advertising_id=${PublisherApp.gaid}&clickid={epoch-time-adid}")
         )
     }
     override fun onCreate(savedInstanceState: Bundle?) {
